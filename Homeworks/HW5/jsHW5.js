3a4a98793b14222bbebc0b77ee181d5054550d30
AnasMu7ammad <anasmu7ammad@hotmail.com>
2022-09-24 08:48:45 +0300

    simple math practice

---
+++ Homeworks/HW5/jsHW5.js
@@ -0,0 +1,26 @@
+document.getElementById("round").innerHTML = Math.round(4.6);
+document.getElementById("round1").innerHTML = Math.round(4.4);
+
+document.getElementById("ceil").innerHTML = Math.ceil(4.9);
+document.getElementById("ceil1").innerHTML = Math.ceil(4.7);
+document.getElementById("ceil2").innerHTML = Math.ceil(4.4);
+document.getElementById("ceil3").innerHTML = Math.ceil(4.2);
+document.getElementById("ceil4").innerHTML = Math.ceil(-4.2);
+
+document.getElementById("floor").innerHTML = Math.floor(4.9);
+document.getElementById("floor1").innerHTML = Math.floor(4.7);
+document.getElementById("floor2").innerHTML = Math.floor(4.4);
+document.getElementById("floor3").innerHTML = Math.floor(4.2);
+document.getElementById("floor4").innerHTML = Math.floor(-4.2);
+
+document.getElementById("trunc").innerHTML = Math.trunc(4.9);
+document.getElementById("trunc1").innerHTML = Math.trunc(4.7);
+document.getElementById("trunc2").innerHTML = Math.trunc(-4.4);
+
+document.getElementById("pow").innerHTML = Math.pow(8,2);
+document.getElementById("pow1").innerHTML = Math.pow(2,8);
+document.getElementById("pow2").innerHTML = Math.pow(-8,2);
+
+document.getElementById("sqrt").innerHTML = Math.sqrt(36);
+
+document.getElementById("abs").innerHTML = Math.abs(-2);
